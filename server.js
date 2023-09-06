b0c430bf55083b8f36b017da69f2646daf7c9a22
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-09-06 07:16:55 -0400

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const app = express()
 const PORT = 3000
 
@@ -6,6 +8,20 @@ app.get("/", (req, res) => {
   res.send("Hello from express")
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.get("/blog", (req, res) => {
+  res.send("I am a Blog")
 })
+
+mongoose
+  .connect(
+    "mongodb+srv://darrickdevelops:Xvkm1LWwbYMDTPnf@projects.mmupqab.mongodb.net/learning?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB")
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })

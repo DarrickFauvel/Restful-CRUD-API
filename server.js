4187e03029f665c05198ec3c6130a05060fb4eb5
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-09-06 06:22:27 -0400

    Setup nodemon

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express")
-
 const app = express()
+const PORT = 3000
 
-app.listen(3000, () => console.log("Hello on port 3000"))
+app.get("/", (req, res) => {
+  res.send("Hello from express")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
